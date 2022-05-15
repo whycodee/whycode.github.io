ca3454be011391a6789ba6327c1320c757a4c7b2
unknown <wahyusaputra3112003@gmail.com>
2022-05-15 07:26:23 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing animation
+    var typed = new Typed(".typing", {
+        strings: ["Student", "Blogger"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Student", "Blogger"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    // window.onscroll = ()=>{
+    //     this.scrollY > 20 ? navbar.classList.add('sticky') : navbar.classList.remove('sticky');
+    // }
+});
